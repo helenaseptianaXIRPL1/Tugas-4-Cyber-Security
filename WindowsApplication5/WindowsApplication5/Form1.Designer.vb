c7d5770a5943f4f46b6481be5edfef8673c2171f
HelenaS <88118215+helenaseptianaXIRPL1@users.noreply.github.com>
2024-11-22 15:47:53 +0700

    Add files via upload

---
+++ WindowsApplication5/WindowsApplication5/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lvProcesses = New System.Windows.Forms.ListView()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lvProcesses
+        '
+        Me.lvProcesses.Location = New System.Drawing.Point(78, 35)
+        Me.lvProcesses.Name = "lvProcesses"
+        Me.lvProcesses.Size = New System.Drawing.Size(649, 252)
+        Me.lvProcesses.TabIndex = 0
+        Me.lvProcesses.UseCompatibleStateImageBehavior = False
+        Me.lvProcesses.View = System.Windows.Forms.View.Details
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Location = New System.Drawing.Point(388, 293)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(75, 23)
+        Me.btnRefresh.TabIndex = 1
+        Me.btnRefresh.Text = "Refresh"
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(367, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Daftar Proses"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(808, 340)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Controls.Add(Me.lvProcesses)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lvProcesses As System.Windows.Forms.ListView
+    Friend WithEvents btnRefresh As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ WindowsApplication5/WindowsApplication5/Form1.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Diagnostics
+
+Public Class Form1
+    ' Event Form Load untuk pertama kali membuka form
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        ' Menyiapkan kolom-kolom ListView
+        lvProcesses.Columns.Add("PID", 100, HorizontalAlignment.Left)
+        lvProcesses.Columns.Add("Name", 200, HorizontalAlignment.Left)
+        lvProcesses.Columns.Add("Status", 100, HorizontalAlignment.Left)
+
+        ' Menampilkan daftar proses pertama kali form dibuka
+        LoadProcesses()
+    End Sub
+
+    ' Fungsi untuk memuat dan menampilkan proses yang sedang berjalan
+    Private Sub LoadProcesses()
+        ' Membersihkan ListView sebelum menambahkan data baru
+        lvProcesses.Items.Clear()
+
+        ' Mengambil semua proses yang sedang berjalan
+        For Each proc As Process In Process.GetProcesses()
+            Try
+                ' Mendapatkan PID, Nama, dan Status Proses
+                Dim pid As Integer = proc.Id
+                Dim name As String = proc.ProcessName
+                Dim status As String = If(proc.Responding, "Running", "Not Responding")
+
+                ' Membuat item baru untuk ListView
+                Dim listViewItem As New ListViewItem(pid.ToString())
+                listViewItem.SubItems.Add(name)
+                listViewItem.SubItems.Add(status)
+
+                ' Menambahkan item ke ListView
+                lvProcesses.Items.Add(listViewItem)
+            Catch ex As Exception
+                ' Menangani error jika ada masalah dengan akses proses
+                ' Tidak menambahkan item ke ListView jika ada kesalahan
+            End Try
+        Next
+
+        
+    End Sub
+
+    ' Event handler untuk tombol Refresh, untuk memuat ulang daftar proses
+    Private Sub btnRefresh_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnRefresh.Click
+        LoadProcesses()
+    End Sub
+End Class
